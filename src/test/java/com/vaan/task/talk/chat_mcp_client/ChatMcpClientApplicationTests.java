42862a1933b1128dcac077b1db6c0f9185760a6a
Imthath <imthath.m@hotmail.com>
2025-04-12 12:12:36 +0530

    spring init

---
+++ src/test/java/com/vaan/task/talk/chat_mcp_client/ChatMcpClientApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vaan.task.talk.chat_mcp_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ChatMcpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

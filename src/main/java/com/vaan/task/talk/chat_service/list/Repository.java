c6c23a99e0c223baca511479d7e24e5f7e1ecae4
Imthath <imthath.m@hotmail.com>
2025-04-12 12:37:54 +0530

    scaffold list

---
+++ src/main/java/com/vaan/task/talk/chat_service/list/Repository.java
@@ -0,0 +1,4 @@
+package com.vaan.task.talk.chat_service.list;
+
+public class Repository {
+}
---
+++ src/main/java/com/vaan/task/talk/chat_service/list/Service.java
@@ -0,0 +1,5 @@
+package com.vaan.task.talk.chat_service.list;
+
+@org.springframework.stereotype.Service
+public class Service {
+}
